43bd5e07d0907485d1342ab09c8dc33615259166
gilbleich <gilbleich@users.noreply.github.com>
2017-05-08 11:02:48 +0300

    Update test.config.ts

--- tests/test.config.ts
+++ tests/test.config.ts
@@ -1,3 +1,3 @@
 
 // export const TEST_URL: string = "file:///D:/workspace/nolio/JavaScript-Sample-Application/index.html";
-export const TEST_URL: string = "http://localhost:9000/";
+export const TEST_URL: string = "ec2-52-215-38-211.eu-west-1.compute.amazonaws.com";
